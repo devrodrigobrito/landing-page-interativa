f3e13a378a190e5f30522c61b684f4cb33192db6
Rodrigo Brito <rodrigobritocontato@gmail.com>
2025-11-18 18:19:28 -0400

    feat(faq): Implementa a lógica de acordeão com fechamento automático

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 const menuHamburgerBtnEl = document.querySelector('#menu-hamburger-btn');
 const navlistEl = document.querySelector('.nav-list');
+const accordionQuestionEl = document.querySelectorAll('.accordion-question');
+const accordionitemEl = document.querySelectorAll('.accordion-item');
 
 
-
-
-
-
-
-menuHamburgerBtnEl.addEventListener('click', function() {
+menuHamburgerBtnEl.addEventListener('click', () => {
     navlistEl.classList.toggle('active');
 });
+
+
+accordionQuestionEl.forEach(question => {
+    question.addEventListener('click', () =>{
+        const item = question.parentElement;
+        item.classList.toggle('active');
+
+        accordionitemEl.forEach(itemLoop => {
+            if(itemLoop !== item){
+                itemLoop.classList.remove('active');
+            }
+        });
+
+    });
+  
+});
+
+
+
